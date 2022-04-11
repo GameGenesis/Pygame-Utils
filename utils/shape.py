1b9be593c6f10a1e1bd91de7b81536e3c450aaa8
Rayan K <gamegenesisdev@gmail.com>
2022-04-11 13:15:00 -0400

    Add docstrings and update return types to Shape classes

--- utils/shape.py
+++ utils/shape.py
@@ -5,7 +5,19 @@ from pygame import Vector2
 
 
 class Shape(object):
-    def __init__(self, position: Vector2=Vector2(0, 0), color: pygame.Color | tuple[int, int, int]=(0, 0, 0)) -> None:
+    def __init__(self, position: Vector2 | tuple[int, int]=Vector2(0, 0), color: pygame.Color | tuple[int, int, int]=(0, 0, 0)) -> None:
+        """
+        Parameters
+        ----------
+        position : Vector2 | tuple[int, int]
+            The position of the shape. Default is (0, 0)
+        color : pygame.Color | tuple[int, int, int]
+            The position of the shape. Default is black or (0, 0, 0)
+
+        Returns
+        -------
+        None
+        """
         self.position = position
         self.color = color
 
@@ -19,13 +31,41 @@ class Shape(object):
 
 
 class Square(Shape):
-    def __init__(self, position: Vector2=Vector2(0, 0), size: Vector2=Vector2(100, 100),
+    def __init__(self, position: Vector2| tuple[int, int]=Vector2(0, 0), size: Vector2 | tuple[int, int]=Vector2(100, 100),
     color: pygame.Color | tuple[int, int, int]=(0, 0, 0)) -> None:
+        """
+        Parameters
+        ----------
+        position : Vector2 | tuple[int, int]
+            The position of the shape. Default is (0, 0)
+        size : Vector2 | tuple[int, int]
+            The size of the square. Default is (100, 100)
+        color : pygame.Color | tuple[int, int, int]
+            The position of the shape. Default is black or (0, 0, 0)
+
+        Returns
+        -------
+        None
+        """
         super().__init__(position, color)
         self.size = size
         self.rect = pygame.Rect(position, size)
 
-    def move(self, x: int=None, y: int=None):
+    def move(self, x: int=None, y: int=None) -> None:
+        """
+        Moves the shape to a specified x and y amount. If either axis is not specified, uses the shape's current position
+
+        Parameters
+        ----------
+        x : int, optional
+            The x position of the object. If not specified, uses the shape's current x position
+        y : int, optional
+            The y position of the object. If not specified, uses the shape's current y position
+
+        Returns
+        -------
+        None
+        """
         if x and y:
             self.position = pygame.Rect(self.position, Vector2(x, y))
         elif x:
@@ -34,15 +74,55 @@ class Square(Shape):
             self.position = pygame.Rect(self.position, Vector2(self.position.x, y))
 
     def draw(self, surface: pygame.Surface):
+        """
+        Draws the shape to a surface
+
+        Parameters
+        ----------
+        surface : Surface
+            The surface to draw the shape on
+
+        Returns
+        -------
+        None
+        """
         pygame.draw.rect(surface=surface, color=self.color, rect=self.rect)
 
 
 class Circle(Shape):
     def __init__(self, position: Vector2=Vector2(0, 0), radius: int=10, color: pygame.Color | tuple[int, int, int]=(0, 0, 0)) -> None:
+        """
+        Parameters
+        ----------
+        position : Vector2 | tuple[int, int]
+            The position of the shape. Default is (0, 0)
+        radius : int
+            The radius of the circle. Default is 10
+        color : pygame.Color | tuple[int, int, int]
+            The position of the shape. Default is black or (0, 0, 0)
+
+        Returns
+        -------
+        None
+        """
         super().__init__(position, color)
         self.radius = radius
 
     def move(self, x: int=None, y: int=None):
+        """
+        Moves the shape to a specified x and y amount. If either axis is not specified, uses the shape's current position
+
+        Parameters
+        ----------
+        x : int, optional
+            The x position of the object. If not specified, uses the shape's current x position
+        y : int, optional
+            The y position of the object. If not specified, uses the shape's current y position
+
+        Returns
+        -------
+        None
+        """
         if x and y:
             self.position = Vector2(x, y)
         elif x:
@@ -51,4 +131,16 @@ class Circle(Shape):
             self.position = Vector2(self.position.x, y)
 
     def draw(self, surface: pygame.Surface):
+        """
+        Draws the shape to a surface
+
+        Parameters
+        ----------
+        surface : Surface
+            The surface to draw the shape on
+
+        Returns
+        -------
+        None
+        """
         pygame.draw.circle(surface=surface, color=self.color, center=self.position, radius=self.radius)
